afdf0aef39cfd9d0aef2176c5e07a9c8ac5ca52a
datboiluiskrrt <datboiluiskrrt@gmail.com>
2022-04-05 16:31:17 -0500

    Working on input forms

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import "./App.css";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
+import SignUpForm from "./components/SignUpForm";
 function App() {
   return (
     <div className="background">
       <Header />
-      <Hero />
+      <div className="registerContainer">
+        <Hero />
+        <SignUpForm />
+      </div>
     </div>
   );
 }
--- src/components/SignUpForm.js
+++ src/components/SignUpForm.js
@@ -7,9 +7,54 @@ const initialValues = {
 };
 
 function Register() {
+  const [formValues, setFormValues] = useState(initialValues);
+
+  const onChange = (event) => {
+    console.log(event.target.value);
+    setFormValues({
+      ...formValues,
+      [event.target.name]: event.target.value,
+    });
+  };
   return (
-    <div>
-      <h2>Register</h2>
+    <div className="register-wrapper">
+      <h2>
+        Get reminded when it’s time to feed that foliage and quench your plant’s
+        thirst.
+      </h2>
+      <form>
+        <div>
+          <label htmlFor="username">Username:</label>
+          <input
+            id="username"
+            name="username"
+            type="text"
+            value={formValues.username}
+            onChange={onChange}
+          ></input>
+        </div>
+        <div>
+          <label htmlFor="password">Password:</label>
+          <input
+            id="password"
+            name="password"
+            type="password"
+            value={formValues.password}
+            onChange={onChange}
+          ></input>
+        </div>
+        <div>
+          <label htmlFor="phoneNumber">Telephone:</label>
+          <input
+            id="phoneNumber"
+            name="phoneNumber"
+            type="tel"
+            pattern="[0-9]{3}-[0-9]{2}-[0-9]{3}"
+            value={formValues.phoneNumber}
+            onChange={onChange}
+          ></input>
+        </div>
+      </form>
     </div>
   );
 }
