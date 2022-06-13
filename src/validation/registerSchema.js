26de9a5c0528db325c997f4952251a336c615a4d
datboiluiskrrt <datboiluiskrrt@gmail.com>
2022-06-13 12:50:49 -0500

    Removed phone validation

--- src/validation/registerSchema.js
+++ src/validation/registerSchema.js
@@ -3,16 +3,6 @@ import * as yup from "yup";
 const SignUpSchema = yup.object().shape({
   username: yup.string().required().min(5),
   password: yup.string().required().min(5),
-  phoneNumber: yup
-    .string()
-    .test(
-      "has-10-digits",
-      "phone number needs to be a 10 digit number",
-      (value) => {
-        const digitsOnlyString = value.replace(/[^0-9]/g, "");
-        return digitsOnlyString.length === 10;
-      }
-    ).required,
 });
 
 export default SignUpSchema;
